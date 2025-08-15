606a668aec67238fa9d62e2937f36732a55b57b8
szitka <szitka@szitka.hu>
2025-08-15 11:19:45 +0200

    Add project files.

---
+++ sztf2.Orokles/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sztf2.Orokles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
